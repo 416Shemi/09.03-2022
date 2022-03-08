75f34e533ae5a1709dcf22eb7c8225088f0fffc8
416Shemi <tu7ngcsvr@code.edu.az>
2022-03-09 01:06:07 +0400

    new projekt 4

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HasLatter("kenan",'0'));
+            Console.ReadLine();
+        }
+        static bool HasLatter(string text,char letter)
+        {
+            for (int i = 0; i <text.Length; i++)
+            {
+                if (text[i] == letter)
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        } 
+    }
+}
